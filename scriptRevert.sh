d47d7bd7b4e21e9f2cb011f998bddb0ec22cc1f7
Giovanni Maggi <gmaggi@deloitte.it>
2021-02-09 22:22:42 +0100

    script wallpaper completo, rimane il revert

--- scriptRevert.sh
+++ scriptRevert.sh
@@ -1,14 +1,18 @@
-now=`date +"%Y-%m-%d-%H:%M:%S"`
+now=`date +"%Y-%m-%d %H:%M:%S"`
 today=`date +"%Y-%m-%d"`
 LOG=log.txt
-JSONLASTTIME=lastRunTime.json
-lastRun=$(jq -r .date pr.json)
 
 echo ${today}' : Initializing the script for reversion of the substitution of the wallpaper.' >> $LOG
 
 cd /Users/gmaggi/Documents/personal-backend/personal-settings/backupDeloitte
-sudo cp ./${lastRun}/DeloitteWallpaper.jpg /var/deloitte/branding/DeloitteWallpaper.jpg
 
-rm lastRunTime.json
+JSONLASTTIME=./lastRunTime.json
+lastRun=(jq -r .lastRun lastRunTime.json)
 
-echo ${today}' : Reverting done with no problem at '${now}'.' >> $LOG
+if [ -f "${JSONLASTTIME}" ] && [ -d "${lastRun}" ]; then
+    sudo cp ./${lastRun}/DeloitteWallpaper.jpg /var/deloitte/branding/DeloitteWallpaper.jpg
+    rm lastRunTime.json
+    echo ${today}' : Reverting done with no problem at '${now}'.' >> $LOG
+else
+    echo ${today}' : There were some problems at retrieving the folder.' >> $LOG
+fi
--- scriptWallpaper.sh
+++ scriptWallpaper.sh
@@ -1,31 +1,52 @@
-now=`date +"%Y-%m-%d-%H:%M:%S"`
+now=`date +"%Y-%m-%d %H:%M:%S"`
 today=`date +"%Y-%m-%d"`
 todayNoMinus=`date +"%Y-%m-%d"`
-LOG=log.txt
-JSONLASTTIME=lastRunTime.json
 
-cd /Users/gmaggi/Documents/personal-backend/personal-settings/backupDeloitte
+cd /Users/gmaggi/Documents/personal-backend/personal-settings/
 
-if [!-f "$LOG"]; then
-    touch log.txt
-    echo ${today}': Initializing the script for the first time at '${now}'.' > $LOG
+LOG=./log.txt
+JSONLASTTIME=./lastRunTime.json
+wallpaperOfChioce=./wallpaper/wallpaperOfChioce.jpg
+
+if [ -f "${wallpaperOfChioce}" ]; then
+    if [ -f "${LOG}" ]; then
+        echo ${now}' | Script started.' >> ${LOG}
     else
-    echo ${today}': Script started at '${now}'.' >> $LOG
+        touch ${LOG}
+        echo ${now}' | Initializing the script for the first time.' > ${LOG}
+    fi
+
+    if [ ! -d "./backupDeloitte/${today}" ]; then
+        mkdir ./backupDeloitte/${today}
+        echo ${now}' | Folder created at .backupDeloitte/'${today}'.' >> ${LOG}
+    fi
+
+    cp /var/deloitte/branding/DeloitteWallpaper.jpg /Users/gmaggi/Documents/personal-backend/personal-settings/backupDeloitte/${today}
+
+    if [ -f "./backupDeloitte/${today}/DeloitteWallpaper.jpg" ]; then
+        echo ${now}' | Wallpaper backed up in the folder .backupDeloitte/'${today}'.' >> $LOG
+        sudo cp ${wallpaperOfChioce} /var/deloitte/branding/DeloitteWallpaper.jpg
+
+        lastModifiedDate=(date -r /var/deloitte/branding/DeloitteWallpaper.jpg +"%Y-%m-%d")
+
+        if [[ ${today}==${lastModifiedDate} ]]; then
+            echo ${now}' | Wallpaper substituted successfully.' >> $LOG
+            echo ${now}' | Script Ended successfully.' >> $LOG
+            echo "{\"lastRun\":\"${today}\"}" > ${JSONLASTTIME}
+        else
+            echo ${now}' | Something went wrong.' >> $LOG
+            echo ${now}' | Script Ended unsuccessfully.' >> $LOG
+        fi
+        else
+            echo ${now}' | No wallpaper backed up in the folder .backupDeloitte/'${today}'.' >> $LOG
+            echo ${now}' | Script ended.' >> $LOG
+    fi
+
+else
+    if [ -f "${LOG}" ]; then
+        echo ${now}' | No wallpaperOfChioce detected.' >> ${LOG}
+    else
+        touch ${LOG}
+        echo ${now}' | No wallpaperOfChioce detected and log.' > ${LOG}
+    fi
 fi
-
-if test !-d ${today} then
-    mkdir ${today}
-    echo ${today}': Folder created at '${now}'.' >> $LOG
-fi
-
-cp /var/deloitte/branding/DeloitteWallpaper.jpg /Users/gmaggi/Documents/personal-backend/personal-settings/backupDeloitte/${today}/DeloitteWallpaper.jpg
-
-echo ${today}': Wallpaper backed up at '${now}' in the folder /Users/gmaggi/Documents/personal-backend/personal-settings/backupDeloitte/'${today}'.' >> $LOG
-
-echo ${today}': Renamed wallpaper '${now}'.' >> $LOG
-
-sudo cp ../wallpaper/wallpaperOfChioce.jpg /var/deloitte/branding/DeloitteWallpaper.jpg
-
-echo ${today}': Wallpaper substituted at '${now}'. Now you can logout and log in with a new wallpaper.' >> ../backupDeloittelog.txt
-
-echo "{\"lastRun\":\"${today}\"}" > $JSONLASTTIME
