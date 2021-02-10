bf105a12313409222aaf9d8661a74bba7e987247
Giovanni Maggi <gmaggi@deloitte.it>
2021-02-10 09:42:33 +0100

    fix scriptwallpaper e sistemazione script revert wallpaper

--- scriptRevert.sh
+++ scriptRevert.sh
@@ -2,17 +2,19 @@ now=`date +"%Y-%m-%d %H:%M:%S"`
 today=`date +"%Y-%m-%d"`
 LOG=log.txt
 
-echo ${today}' : Initializing the script for reversion of the substitution of the wallpaper.' >> $LOG
+echo ${today}' | [SCRIPT REVERT WALLPAPER] | Script Started.' >> $LOG
+echo ${today}' | [SCRIPT REVERT WALLPAPER] | Initializing the script for revertion of the substitution of the wallpaper.' >> $LOG
 
 cd /Users/gmaggi/Documents/personal-backend/personal-settings/backupDeloitte
 
 JSONLASTTIME=./lastRunTime.json
 lastRun=`(jq -r .lastRun lastRunTime.json)`
 
-if [ -f "${JSONLASTTIME}" ] && [ -d "${lastRun}" ]; then
+if [ -f "${JSONLASTTIME}" ] && [ -d "${lastRun}" ] && [ -f "./${lastRun}/DeloitteWallpaper.jpg" ]; then
     sudo cp ./${lastRun}/DeloitteWallpaper.jpg /var/deloitte/branding/DeloitteWallpaper.jpg
-    rm lastRunTime.json
-    echo ${today}' : Reverting done with no problem at '${now}'.' >> $LOG
+    echo ${today}' | [SCRIPT REVERT WALLPAPER] | Reverting done with no problem at '${now}'.' >> $LOG
 else
-    echo ${today}' : There were some problems at retrieving the folder.' >> $LOG
+    echo ${today}' | [SCRIPT REVERT WALLPAPER] | There were some problems at retrieving the folder.' >> $LOG
 fi
+
+echo ${today}' | [SCRIPT REVERT WALLPAPER] | Script Ended.' >> $LOG
--- scriptWallpaper.sh
+++ scriptWallpaper.sh
@@ -13,47 +13,47 @@ wallpaperOfChioce=./wallpaper/wallpaperOfChioce.jpg
 if [[ ${modifiedDateTime} != ${modifiedDateTimeFromJson} ]]; then
     if [ -f "${wallpaperOfChioce}" ]; then
         if [ -f "${LOG}" ]; then
-            echo ${now}' | Script started.' >> ${LOG}
+            echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Script started.' >> ${LOG}
         else
             touch ${LOG}
-            echo ${now}' | Initializing the script for the first time.' > ${LOG}
+            echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Initializing the script for the first time.' > ${LOG}
         fi
 
         if [ ! -d "./backupDeloitte/${today}" ]; then
             mkdir ./backupDeloitte/${today}
-            echo ${now}' | Folder created at .backupDeloitte/'${today}'.' >> ${LOG}
+            echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Folder created at .backupDeloitte/'${today}'.' >> ${LOG}
         fi
 
         cp /var/deloitte/branding/DeloitteWallpaper.jpg /Users/gmaggi/Documents/personal-backend/personal-settings/backupDeloitte/${today}
 
         if [ -f "./backupDeloitte/${today}/DeloitteWallpaper.jpg" ]; then
-            echo ${now}' | Wallpaper backed up in the folder .backupDeloitte/'${today}'.' >> $LOG
+            echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Wallpaper backed up in the folder .backupDeloitte/'${today}'.' >> $LOG
             sudo cp ${wallpaperOfChioce} /var/deloitte/branding/DeloitteWallpaper.jpg
 
             lastModifiedDate=`(date -r /var/deloitte/branding/DeloitteWallpaper.jpg +"%Y-%m-%d")`
             lastModifiedDateTime=`(date -r /var/deloitte/branding/DeloitteWallpaper.jpg +"%Y-%m-%d %H:%M:%S")`
 
             if [[ ${today} == ${lastModifiedDate} ]]; then
-                echo ${now}' | Wallpaper substituted successfully.' >> $LOG
-                echo ${now}' | Script Ended successfully.' >> $LOG
+                echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Wallpaper substituted successfully.' >> $LOG
+                echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Script Ended successfully.' >> $LOG
                 echo "{\"lastRun\":\"${today}\", \"lastModifiedDateTime\":\"${lastModifiedDateTime}\"}" > ${JSONLASTTIME}
             else
-                echo ${now}' | Something went wrong.' >> $LOG
-                echo ${now}' | Script Ended unsuccessfully.' >> $LOG
+                echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Something went wrong.' >> $LOG
+                echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Script Ended unsuccessfully.' >> $LOG
             fi
             else
-                echo ${now}' | No wallpaper backed up in the folder .backupDeloitte/'${today}'.' >> $LOG
-                echo ${now}' | Script ended.' >> $LOG
+                echo ${now}' | [SCRIPT CHANGE WALLPAPER] | No wallpaper backed up in the folder .backupDeloitte/'${today}'.' >> $LOG
+                echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Script ended.' >> $LOG
         fi
 
     else
         if [ -f "${LOG}" ]; then
-            echo ${now}' | No wallpaperOfChioce detected.' >> ${LOG}
+            echo ${now}' | [SCRIPT CHANGE WALLPAPER] | No wallpaperOfChioce detected.' >> ${LOG}
         else
             touch ${LOG}
-            echo ${now}' | No wallpaperOfChioce detected and log.' > ${LOG}
+            echo ${now}' | [SCRIPT CHANGE WALLPAPER] | No wallpaperOfChioce detected and log.' > ${LOG}
         fi
     fi
 else
-    echo ${now}' | Last modified wallpaper is made by this script.' >> ${LOG}
+    echo ${now}' | [SCRIPT CHANGE WALLPAPER] | Last modified wallpaper is made by this script.' >> ${LOG}
 fi
