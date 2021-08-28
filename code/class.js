5d8b01df1fedf31dc209e96f80f90e8cba54a9d1
Giovanni Maggi <gvnnmaggi@gmail.com>
2021-08-28 08:35:29 +0200

    prototype and something else

--- code/class.js
+++ code/class.js
@@ -51,3 +51,110 @@ student.Id
 
 student.id = 891
 student.Id
+
+class Vehicle {
+  constructor(age, brand) {
+    this.age = age,
+    this.brand = brand
+  }
+
+  get Age() {
+    return this.age
+  }
+  get Brand() {
+    return this.brand
+  }
+}
+
+class Bike extends Vehicle {
+  constructor(age, brand, tyreWidth, size, maxRearGear, maxFrontGear, minRearGear, rearGear) {
+    super(age, brand),
+    this.tyreWidth = tyreWidth,
+    this.size = size
+    this.maxRearGear = maxRearGear
+    this.maxFrontGear = maxFrontGear
+    this.minRearGear = minRearGear
+    this.rearGear = rearGear
+  }
+
+  get TyreWidth() {
+    return this.tyreWidth
+  }
+  get Size() {
+    return this.size
+  }
+  get RearGear() {
+    return this.rearGear
+  }
+
+  calculateRollingResistence() {
+    let randomWeatherInfo = getRandomWeatherInfo()
+    let ratio = randomWeatherInfo.ratio * this.tyreWidth
+    console.log(`The rolling resistance given by this day of ${randomWeatherInfo.wheater} is ${ratio.toFixed(2)}`)
+  }
+
+  changeRearGear() {
+    if(this.rearGear === this.maxRearGear) {
+      this.rearGear += 1
+      console.log('gear changed down')
+    } else {
+      this.rearGear -= 1
+      console.log('gear changed up')
+    }
+  }
+}
+
+
+getRandomWeatherInfo = function() {
+  let ratio = Math.random().toFixed(2)
+  let wheater = 'sunny'
+  if(ratio >= 0.3 && ratio < 0.7) {
+    wheater = 'cloudy'
+  } else if(ratio >= 0.7) {
+    wheater = 'rainy'
+  }
+  return {ratio : ratio, wheater : wheater}
+}
+
+let bike = new Bike(2, 'giant', 25, 54, 11, 53, 30, 15)
+let bike2 = new Bike(2, 'bmc', 25, 54, 12, 52, 32, 9)
+bike.calculateRollingResistence()
+Bike.prototype.calculateWheelSpinLength = function() {
+  console.log('this.maxRearGear', this.maxRearGear)
+  let lengthSpinRatio = this.maxFrontGear / this.maxRearGear
+  let lengthSpin = ((lengthSpinRatio) * Math.PI * 700).toFixed(2)
+  console.log(`The length of a wheel spin is ${lengthSpin} in millimeters, hence spinned ${lengthSpinRatio.toFixed(2)} times. We are talking about the ${this.brand}`)
+}
+bike.calculateWheelSpinLength()
+bike2.calculateWheelSpinLength()
+
+bike.changeRearGear()
+console.log('bike gear of the first bike, before protorype', bike.RearGear)
+bike2.changeRearGear()
+console.log('bike gear of the second bike, before protorype', bike2.RearGear)
+
+Bike.prototype.changeRearGear = function() {
+  if(this.rearGear === this.maxRearGear) {
+    this.rearGear += 1
+    console.log('gear changed down')
+  } else if(this.rearGear === this.minRearGear) {
+    this.rearGear -= 1
+    console.log('gear changed up')
+  } else if(this.rearGear > this.maxRearGear && this.rearGear < this.minRearGear ) {
+    let ranomchoice = Math.random()
+    if(ranomchoice < 0.5) {
+      this.rearGear -= 1
+      console.log('gear changed up')
+    } else {
+      this.rearGear += 1
+      console.log('gear changed down')
+    }
+  } else {
+    console.log('the gear is broken')
+  }
+}
+
+bike.changeRearGear()
+console.log('bike gear of the first bike, after protorype', bike.RearGear)
+bike2.changeRearGear()
+console.log('bike gear of the second bike, after protorype', bike2.RearGear)
